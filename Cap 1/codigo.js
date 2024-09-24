569c034713a36acf771d2f7e14aa96e329581af9
Nicolas <nicolasdelponte2012@gmail.com>
2024-09-24 11:52:46 -0300

    Resubida

---
+++ Cap 1/codigo.js
@@ -0,0 +1,73 @@
+dinerocofla = prompt("cuanto tiene cofla");
+dineroRoberto = prompt("cuanto tiene robert");
+dineropedro = prompt("cuanto tiene pedro");
+
+
+if(dinerocofla>0.6 && dinerocofla<1){
+alert("Cofla: comprate el de agua");
+}
+else if(dinerocofla>=1 && dinerocofla<1.6){
+    alert("Cofla: comprate el de agua2");
+    }
+else if(dinerocofla>1.6 && dinerocofla<1.7){
+alert("Cofla: comprate el de heladix");
+}
+else if(dinerocofla>=1.7 && dinerocofla <1.8){
+    alert("Cofla: comprate el de heladovich");
+}
+else if (dinerocofla>=1.8 && dinerocofla <2.9){
+    alert("Cofla: comprate el de helardo");
+}
+else if (dinerocofla>=2.9){
+alert("Cofla: helado confite");
+}
+else{
+    alert("Cofla: lo siento pero sos pobre");
+}
+
+
+if(dineroRoberto>0.6 && dineroRoberto<1){
+    alert("Roberto: comprate el de agua");
+    }
+    else if(dineroRoberto>=1 && dineroRoberto<1.6){
+        alert("Roberto: comprate el de agua2");
+        }
+    else if(dineroRoberto>1.6 && dineroRoberto<1.7){
+    alert("Roberto: comprate el de heladix");
+    }
+    else if(dineroRoberto>=1.7 && dineroRoberto <1.8){
+        alert("Roberto: comprate el de heladovich");
+    }
+    else if (dineroRoberto>=1.8 && dineroRoberto <2.9){
+        alert("Roberto: comprate el de helardo");
+    }
+    else if (dineroRoberto>=2.9){
+    alert("Roberto: helado confite");
+    }
+    else{
+        alert("Roberto: lo siento pero sos pobre");
+    }
+
+
+    
+if(dineropedro>0.6 && dineropedro<1){
+    alert("Pedro: comprate el de agua");
+    }
+    else if(dineropedro>=1 && dineropedro<1.6){
+        alert("Pedro: comprate el de agua2");
+        }
+    else if(dineropedro>1.6 && dineropedro<1.7){
+    alert("Pedro: comprate el de heladix");
+    } 
+    else if(dineropedro>=1.7 && dineropedro <1.8){
+        alert("Pedro: comprate el de heladovich");
+    }
+    else if (dineropedro>=1.8 && dineropedro <2.9){
+        alert("Pedro: comprate el de helardo");
+    }
+    else if (dineropedro>=2.9){
+    alert("Pedro: helado confite");
+    }
+    else{
+        alert("Pedro: lo siento pero sos pobre");
+    }
---
+++ Cap2/codigo.js
@@ -0,0 +1,84 @@
+
+// ARRAYS ASOC.
+/*
+let pc1 = {
+    nombre: "daltopc",
+    procesador: "intel corei7",
+    ram:"16GB",
+    espacio: "1TB"
+};
+let nombre = pc1["nombre"];
+let procesador = pc1["procesador"];
+let ram = pc1["ram"];
+let espacio = pc1["espacio"];
+
+frase = `
+    el nombre de mi PC es: <b>${nombre}</b> <br>
+    el procesador es: <b>${procesador}</b> <br>
+    la memoria ram es: <b>${ram}</b> <br>
+    el espacio en disco es de <b>${espacio}</b>`;
+
+document.write(frase);
+*/
+/*
+for(let i = 0; i<6; i++){
+    document.write(i + "<br>")
+    //continue - Corta ESTA iteracion del for pero sigue con las otras
+
+    //break - Rompe todo el bucle y finaliza el for
+
+
+}
+    */
+ /*
+let animales=["gato", "perro", "tiranosaurio rex"]
+animales.edad=20;
+
+for (animal in animales){
+    document.write(animal + "<br>");
+}
+
+document.write("<br>");
+
+for(animal of animales){
+    document.write(animal + "<br>")
+}
+
+array1 = ["maría", "josefa", "roberta"];
+array2 = ["pedro", "marcelo", array1, "josefina"];
+
+forRandom:
+for (let array in array2) {
+    if (array == 2) {
+        for (let array of array1) {
+            if (array == "maría") {
+                continue forRandom;
+            }
+            document.write(array + "<br>");
+        }
+    } else {
+        document.write(array2[array] + "<br>");
+    }
+}
+    */
+
+/*
+   function Saludar(){
+    respuesta = prompt("hola");
+    if(respuesta == "hola"){
+        alert("hola");
+        return "se ejecuto hola";
+    }else{
+        alert("chau");
+        return "se ejecuto chau";
+    }
+
+   }
+
+   
+document.write(Saludar);
+
+document.write(Saludar());
+
+*/
+
