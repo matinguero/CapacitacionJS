5afdfa493f932066d1d64aa1db2f272ff1674040
Nicolas <nicolasdelponte2012@gmail.com>
2024-10-03 17:17:12 -0300

    update

--- proyectito pokemon/codigo.js
+++ proyectito pokemon/codigo.js
@@ -18,38 +18,7 @@ $(document).ready(function() {
                 let URLSpecies = response.species;
 
 
-                $.ajax({
-                    url: URLSpecies.url ,
-                    type: "GET",
-                    success: function(response){
-                        let evochain = response.evolution_chain;
-
-                        $.ajax({
-                            url: evochain.url,
-                            type: "GET",
-                            success: function(response){
-                                let evolves = response.chain.evolves_to[0].evolves_to[0].species.name;
-
-                               console.log(evolves);
-                               $("#ddlEvos").append($('<option>', {
-                                value: 1,
-                                text: evolves
-                            }));
-                            },
-                            error: function(error){
-                                console.log("ERROR: " + error);
-                            }
-                            
-                        })
-
-
-
-                    },
-                    error: function(error){
-                        console.log("ERROR: " + error);
-                    }
-
-                })
+                
                 // Display fetched information in the div
                 $("#pokemon2").text(nombrepokemon);
                 $("#imgPokemonF").attr("src", img.front_default);
@@ -57,7 +26,7 @@ $(document).ready(function() {
                 
                 $("#hid").show();
                 console.log("Atrapaste un: " + nombrepokemon + "!")
-                console.log(habilidades);
+                
                 for (let i = 0; i<habilidades.length; i++){
                     console.log(habilidades);
                     $('#ulPokemon').append('<li>' + habilidades[i].ability.name + '</li>');
