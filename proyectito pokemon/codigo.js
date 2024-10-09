84ced98a8c97f95ea1039870e458d3c620b11276
Nicolas <nicolasdelponte2012@gmail.com>
2024-10-09 11:17:46 -0300

    Subida atrasada

--- proyectito pokemon/codigo.js
+++ proyectito pokemon/codigo.js
@@ -1,27 +1,36 @@
 
-$(document).ready(function() {
-    flag=true;
+$(document).ready(function() {""
+    
     let numero = Math.floor(Math.random() * 1000);
-    let botonnexturl
-    let botonback
+    let botonnexturl;
+    let botonback;
+
+
+    CargarTabla("https://pokeapi.co/api/v2/pokemon/");
   
-    cargarpokemones();
-
     cargartarjetapokemon(numero);
-
-
 
     $("#btnNext").click(function() {
 
-        botonNext();
-       
+        CargarTabla( $("#btnNext").attr("url"));
+
     });
 
+    $('#pokedex td').on('click', function() {
+        // Get the text of the clicked cell
+        var cellData = $(this).text();
+        
+        // Log the data to the console or use it as needed
+        console.log("Cell data: " + cellData);
+        
+        // Example: Display the data in an alert
+        alert("You clicked on: " + cellData);
+    });
 
     $("#btnBack").click(function() {
         
-        botonBack();
-            
+        CargarTabla($("#btnBack").attr("url"));
+
     });
 
     
@@ -41,49 +50,6 @@ $(document).ready(function() {
 });
 
 
-  function cargarpokemones() {
-    $.ajax({
-        url: "https://pokeapi.co/api/v2/pokemon/",
-        type: "GET",
-        success: function(response) {
-                let pokemones = response.results;
-                botonnexturl = response.next;
-                botonback = response.previous;
-                if(botonback === null){
-                    $("#btnBack").prop("disabled", true);
-                }else{
-                    $("#btnBack").prop("disabled", false);
-                }
-                if(botonNext === null){
-                    $("#btnNext").prop("disabled", true);
-                }else{
-                    $("#btnNext").prop("disabled", false);
-                }
-                console.log( pokemones);
-                let nombrenro = 0;
-                for (let i = 0; i < 4; i++) {
-
-                   const row = $('<tr></tr>');
-                    
-                    for (let j = 0; j < 5; j++) {
-                       
-                        const pos = $('<td></td>').text(pokemones[nombrenro].name);
-                        row.append(pos); 
-                        
-                        nombrenro++; 
-                    }
-            
-                   
-                    $('#pokedex').append(row);
-                }
-
-        },
-        error: function(error) {
-            console.log("Error:", error);
-        }
-    })
-}
-
 function cargartarjetapokemon(id) {
     $.ajax({
         url: " https://pokeapi.co/api/v2/pokemon/" + id,
@@ -94,11 +60,12 @@ function cargartarjetapokemon(id) {
             let habilidades = response.abilities;
             let img = response.sprites;
             let URLSpecies = response.species;
-
             
             $("#pokemon2").text(nombrepokemon);
             $("#imgPokemonF").attr("src", img.front_default);
-            $("#imgPokemonB").attr("src", img.back_default);            
+            $("#imgPokemonB").attr("src", img.back_default);      
+            $("#imgPokemonF").attr("alt", "Imagen delantera de " + nombrepokemon);
+            $("#imgPokemonB").attr("alt", "Imagen trasera de " + nombrepokemon);                  
             $("#hid").show();
 
             console.log("Atrapaste un: " + nombrepokemon + "!")
@@ -118,100 +85,50 @@ function cargartarjetapokemon(id) {
 
 }
 
-
-function botonNext(){
+function CargarTabla(URL){
     $.ajax({
-        url: botonnexturl,
+        url: URL,
         type: "GET",
         success: function(response) {
 
                 $("#pokedex").empty();
             
                 let pokemones = response.results;
-                botonnexturl = response.next;
-                botonback = response.previous;
-            
-                if(botonnexturl === null){
+                if (response.next===null){
                     $("#btnNext").prop("disabled", true);
+                    $("#btnNext").attr("url", "");
                 }else{
                     $("#btnNext").prop("disabled", false);
+                    $("#btnNext").attr("url", response.next);
                 }
-                if(botonback === null){
+                if (response.previous===null){
                     $("#btnBack").prop("disabled", true);
+                    $("#btnBack").attr("url", "");
                 }else{
                     $("#btnBack").prop("disabled", false);
+                    $("#btnBack").attr("url", response.previous);
                 }
+               
+               
+            
                 
                 console.log( pokemones);
-                let nombrenro = 0;
+                let tabla;
+                let nombrenro = 0; //REMOVER
                 for (let i = 0; i < 4; i++) {
 
                    const row = $('<tr></tr>');
                     
                     for (let j = 0; j < 5; j++) {
                        
-                        const pos = $('<td></td>').text(pokemones[nombrenro].name);
+                        const pos = $('<td></td>').text(pokemones[nombrenro].name); //agregar campo hidden con URL
                         row.append(pos); 
                         
                         nombrenro++; 
                     }
             
-                   
-                    $('#pokedex').append(row);
+                $("#pokedex").append(row);
                 }
-
-        },
-        error: function(error) {
-            console.log("Error:", error);
-        }
-    })
-
-}
-
-function botonBack(){
-
-    $.ajax({
-        url: botonback,
-        type: "GET",
-        success: function(response) {
-            
-                $("#pokedex").empty();
-                
-                let pokemones = response.results;
-                botonnexturl = response.next;
-                botonback = response.previous;
-                
-                if(botonback === null){
-                    $("#btnBack").prop("disabled", true);
-                }else{
-                    $("#btnBack").prop("disabled", false);
-                }
-                if(botonnexturl === null){
-                    $("#btnNext").prop("disabled", true);
-                }else{
-                    $("#btnNext").prop("disabled", false);
-                }
-                
-                console.log( pokemones);
-                
-                let nombrenro = 0;
-                
-                for (let i = 0; i < 4; i++) {
-
-                   const row = $('<tr></tr>');
-                    
-                    for (let j = 0; j < 5; j++) {
-                       
-                        const pos = $('<td></td>').text(pokemones[nombrenro].name);
-                        row.append(pos); 
-                        
-                        nombrenro++; 
-                    }
-            
-                   
-                    $('#pokedex').append(row);
-                }
-
         },
         error: function(error) {
             console.log("Error:", error);
